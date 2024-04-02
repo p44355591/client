58f1a5042f77edb6ad6bbba5a0d60ac594fd0b49
בתיה בלאו < @m08.local>
2024-04-02 21:51:05 +0300

    login fileds

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,33 +1,55 @@
 
-// import TextField from '@mui/material/TextField';
-// import './style.css'
-// import Button from '@mui/material/Button';
-// import { useState } from 'react';
+import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import './style.css'
+import Button from '@mui/material/Button';
 
-// const [firsName, setfirstName] = useState("");
-// const [lastName, setlastName] = useState("");
-// const [phone, setphone] = useState("");
-// const [email, setemail] = useState("");
-// const [userName, setuserName] = useState("");
-// const [password, setpassword] = useState("");
 
-// const enterValues=()=>{
-//     console.log('הכנס נתונים לדטה בייס')
-// }
 
-// const handlePassword = (event) => {
-//     setPassword(event.target.value)
-// }
+export const Login = () => {
+    const [firsName, setfirstName] = useState("");
+    const [lastName, setlastName] = useState("");
+    const [phone, setphone] = useState("");
+    const [email, setemail] = useState("");
+    const [userName, setuserName] = useState("");
+const [password, setpassword] = useState("");
 
-// export const Login = () => {
-//     return (<div className='enter-container'>
-//         <TextField id="outlined-basic" label="שם פרטי" variant="outlined" value={firsName}  onChange={handleUserName}/>
-//         <TextField id="outlined-basic" label="שם משפחה" variant="outlined" value={lastName} onChange={handleUserName} />
-//         <TextField id="outlined-basic" label="טלפון " variant="outlined" value={phone} onChange={handleUserName}/>
-//         <TextField id="outlined-basic" label="מייל " variant="outlined"  value={email} onChange={handleUserName}/>
-//         <TextField id="outlined-basic" label="שם משתמש" variant="outlined"  value={userName} onChange={handleUserName}/>
-//         <TextField id="outlined-basic" label="סיסמא " variant="outlined"value={password} onChange={handleUserName}/>
-//         <Button variant="outlined" onClick={()=>enterValues()}>הכנס</Button>
+const enterValues=()=>{
+    console.log('הכנס נתונים לדטה בייס')
+}
 
-//     </div>)
-// }
+const handleFirstName = (event) => {
+    setfirstName(event.target.value)
+}
+
+const handleLastName = (event) => {
+    setlastName(event.target.value)
+}
+
+const handlePhone = (event) => {
+    setphone(event.target.value)
+}
+
+const handleEmail = (event) => {
+    setemail(event.target.value)
+}
+
+const handleUserName = (event) => {
+    setuserName(event.target.value)
+}
+
+const handlepassword = (event) => {
+    setpassword(event.target.value)
+}
+
+    return (<div className='enter-container'>
+        <TextField id="outlined-basic" label="שם פרטי" variant="outlined" value={firsName}  onChange={handleFirstName}/>
+        <TextField id="outlined-basic" label="שם משפחה" variant="outlined" value={lastName} onChange={handleLastName} />
+        <TextField id="outlined-basic" label="טלפון " variant="outlined" value={phone} onChange={handlePhone}/>
+        <TextField id="outlined-basic" label="מייל " variant="outlined"  value={email} onChange={handleEmail}/>
+        <TextField id="outlined-basic" label="שם משתמש" variant="outlined"  value={userName} onChange={handleUserName}/>
+        <TextField id="outlined-basic" label="סיסמא " variant="outlined"value={password} onChange={handlepassword}/>
+        <Button variant="outlined" onClick={()=>enterValues()}>הכנס</Button>
+
+    </div>)
+}
